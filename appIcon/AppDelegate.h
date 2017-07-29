7fae346eeb3e5729cdf87529a7fd5665796b9459
Marshal <921871381@qq.com>
2017-07-29 17:12:57 +0800

    Initial Commit

---
+++ appIcon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appIcon
+//
+//  Created by 王禄宪 on 2017/7/29.
+//  Copyright © 2017年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appIcon/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  appIcon
+//
+//  Created by 王禄宪 on 2017/7/29.
+//  Copyright © 2017年 Marshal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
